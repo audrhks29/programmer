7637d288ab1ea8dd9fe992bbb898081fb49068f8
audrhks29 <audrhks29@gmail.com>
2024-03-05 17:06:15 +0900

    first commit

---
+++ level_0/181935/181935.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = 0;
+  let odd = 0;
+  let even = 0;
+  let num = n;
+  for (n; n >= 1; n--) {
+    n % 2 === 0 ? even += n ** 2 : odd += n
+  }
+  answer = num % 2 === 0 ? even : odd
+  return answer;
+}
+solution(10)
